e925b2865e7374a1563a6499e0c80ed959b84d93
cskaoyan-java <zhangshuai@cskaoyan.onaliyun.com>
2023-12-27 17:30:33 +0800

    基础语法-1

---
+++ 01- JavaSE/01-代码/src/baisc/identifier/Demo1.java
@@ -0,0 +1,23 @@
+package baisc.identifier;
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+
+        int a1 = 1;
+        int A = 1;
+        int _A = 3;
+        int $a = 1;
+        int 你 = 1;
+        int の = 1;
+
+        // 数字字符不能打头
+        //int 1a = 1;
+
+        // 严格区分大小写
+        int b = 1;
+        int B = 2;
+
+        int headinqueueoffile = 1;
+    }
+}
